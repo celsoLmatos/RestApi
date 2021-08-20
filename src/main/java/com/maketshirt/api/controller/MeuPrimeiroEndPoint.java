a4e2c02fdf39fce189c66124b7eed3d0c7731ab8
celsoLmatos <celsoluis.matos@gmail.com>
2021-08-20 14:18:02 -0300

    Criação do primeiro End Point

--- src/main/java/com/maketshirt/api/controller/MeuPrimeiroEndPoint.java
+++ src/main/java/com/maketshirt/api/controller/MeuPrimeiroEndPoint.java
@@ -1,14 +1,15 @@
 package com.maketshirt.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MeuPrimeiroEndPoint {
 	
-	@GetMapping("/ola")
-	public String ola() {
-		return "Olá, Bom dia !!";
+	@GetMapping("/")
+	public String PrimeroEndPoint() {
+		return "Meu primeiro EndPoint";
 	}
 
 }
